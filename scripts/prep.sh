f28f29b5df159a8150e1b45f4a4827640273743c
Gavin M. Roy <gavinr@aweber.com>
2015-02-11 14:27:04 -0500

    Remove a 100k logfile from the distribution of the app

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -17,3 +17,6 @@ rm ./binaries/Vessel.app/Contents/Resources/atom.icns
 
 echo "Making app dir"
 mkdir -p ./binaries/Vessel.app/Contents/Resources/app
+
+echo "Removing dependency logfile"
+rm node_modules/moment/sauce_connect.log
