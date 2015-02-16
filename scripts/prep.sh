29acad99afb1839a3359814a8c7fca70736b99ff
Gavin M. Roy <gavinr@aweber.com>
2015-02-16 12:28:31 -0500

    Uncomment the echo

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -9,5 +9,5 @@ rm ./binaries/Vessel.app/Contents/Resources/atom.icns
 echo "Making app dir"
 mkdir -p ./binaries/Vessel.app/Contents/Resources/app
 
-#echo "Removing dependency logfile"
+echo "Removing dependency logfile"
 rm -f node_modules/moment/sauce_connect.log
